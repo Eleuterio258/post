620972eaf9206df42d6138e1db6cfb1e7e9fe0e7
Eleuterio258 <eleuterio3d@gmail.com>
2023-01-02 09:48:43 +0200

    ROTAS

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\PostController;
 
 
 Route::get('posts', [PostController::class, 'index']);
-Route::get('post/{id}', [PostController::class, 'show']);
-Route::post('post', [PostController::class, 'store']);
-Route::put('post/{id}', [PostController::class, 'update']);
-Route::delete('post/{id}', [PostController::class, 'destroy']);
+Route::get('posts/{id}', [PostController::class, 'show']);
+Route::post('posts', [PostController::class, 'store']);
+Route::put('posts/{id}', [PostController::class, 'update']);
+Route::delete('posts/{id}', [PostController::class, 'destroy']);
